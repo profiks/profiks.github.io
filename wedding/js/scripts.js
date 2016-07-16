beb9a85f4c525a83f411fc1f8712da3c3a5b8e49
profiks <denys.ark@gmail.com>
2016-07-16 13:46:48 +0300

    all done

--- wedding/js/scripts.js
+++ wedding/js/scripts.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
     
-    $('h1').fitText(1.2, { minFontSize: '25px', maxFontSize: '42px' });
+    $('h1.text-blue').fitText(1.2, { minFontSize: '25px', maxFontSize: '42px' });
     $('h2').fitText(1.2, { minFontSize: '22px', maxFontSize: '34px' });
+    $('h3.header-big').fitText(1.2, { minFontSize: '22px', maxFontSize: '34px' });
     $('h2.text-med').fitText(1.2, { minFontSize: '20px', maxFontSize: '25px' });
     $('h4').fitText(1.2, { minFontSize: '18px', maxFontSize: '23px' });
     
@@ -31,6 +32,30 @@ $( document ).ready(function() {
                        email: ""    
                     }
 				}
+    });
+    
+    
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:5,
+                nav:true,
+                loop:false
+            }
+        },
+        nav: true,
+        navText: ["<span class='icon-l'><</span>","<span class='icon-r'>></span>"]
     });
     
     
