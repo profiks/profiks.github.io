df43361ee212ebf27300f4c18fa0708b10c15704
profiks <denys.ark@gmail.com>
2016-08-22 23:08:03 +0300

    mob

--- works/yalchin-zahid/js/src/modules/backgroundAudio.js
+++ works/yalchin-zahid/js/src/modules/backgroundAudio.js
@@ -18,34 +18,44 @@ export var backgroundAudio = {
         this.toggle = document.getElementById('bgAudion');
         this.click = ('ontouchstart' in window ? 'touchstart' : 'click');
         var audio = new Audio('../audio/background-music.mp3');
-      
+        
             if(this.toggle){
+                
+                if(screen.width < 500 || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) {
+               
+                    this.toggle.style.display = "none";
+                    
+                }else{
+                   
+                    audio.volume = 0.10;
+                    audio.loop = true;
+                    self.fadeAudio(audio);
+                    audio.play();
+
+                    this.toggle.classList.add('equalizer--play');
+
+
+                    this.toggle.addEventListener(this.click, () => {
+
+                        if(audio.paused){
+
+                            audio.volume = 0.10;
+                            self.fadeAudio(audio);
+                            audio.play();
+
+                            this.toggle.classList.add('equalizer--play');
+
+                        } else {
+                            audio.volume = 0.10;
+                            audio.pause();
+                            this.toggle.classList.remove('equalizer--play');                     
+                        }
+
+                    }, false);
+                    
+                }
             
-                audio.volume = 0.10;
-                audio.loop = true;
-                self.fadeAudio(audio);
-                audio.play();
-
-                this.toggle.classList.add('equalizer--play');
-
-
-                this.toggle.addEventListener(this.click, () => {
-
-                    if(audio.paused){
-
-                        audio.volume = 0.10;
-                        self.fadeAudio(audio);
-                        audio.play();
-
-                        this.toggle.classList.add('equalizer--play');
-
-                    } else {
-                        audio.volume = 0.10;
-                        audio.pause();
-                        this.toggle.classList.remove('equalizer--play');                     
-                    }
-
-                }, false);
+                
 
             }// end if
                 
