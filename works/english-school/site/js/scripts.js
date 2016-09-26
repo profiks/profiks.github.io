f912474eb41e2e509eecc2d6c5168b5eba624137
profiks <denys.ark@gmail.com>
2016-09-26 13:07:00 +0300

    new

--- works/english-school/site/js/scripts.js
+++ works/english-school/site/js/scripts.js
@@ -6,17 +6,32 @@ $(document).ready(function(){
     /**
     * Fixed top navigation and aside form
     * @use jquery sticky plugin : site/js/jquery.sticky.js
-    */
-    $("#stickyNavigation").sticky({topSpacing:0});
+    */    
+    if ($(window).width() >= 768) {
+        $("#stickyNavigation").sticky({topSpacing:0});
+        $("#stickyNavigation").removeClass('navbar-fixed-top');
+    }else{
+        $("#stickyNavigation").addClass('navbar-fixed-top');
+    }
+    
+    $(window).resize(function(){
+        if ($(window).width() >= 768) {
+           $("#stickyNavigation").sticky({topSpacing:0});
+           $("#stickyNavigation").removeClass('navbar-fixed-top');
+        }else{
+            $("#stickyNavigation").addClass('navbar-fixed-top');
+            $("#stickyNavigation").unstick();
+        }
+    });
     
     
     /**
     * News feed on banner bottom
     * @use owl carousel plugin : site/js/owl.carousel.min.js
     */
-    var owl = $('#bannerCourses');
+    var banner = $('#bannerCourses');
     
-    owl.owlCarousel({
+    banner.owlCarousel({
         loop:true,
         nav:true,
         navText : ['',''],
@@ -26,16 +41,107 @@ $(document).ready(function(){
                 items:1
             },
             600:{
-                items:2
+                items:1
             },            
             960:{
-                items:3
+                items:1
             },
             1200:{
-                items:4
+                items:1
             }
         }
     });
+    
+    
+    /**
+    * Program courses carousel enabled only on mobile and thablet
+    * @use owl carousel plugin : site/js/owl.carousel.min.js
+    */
+    
+    var program = $('#programsMob');
+    
+    if ($(window).width() <= 767) {         
+        program.owlCarousel({
+            navigation : true,
+            items:1
+        });        
+    }
+    
+    $(window).resize(function(){
+        if (($(window).width() >= 767) && (program.hasClass('owl-carousel'))) {
+            program.trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+            program.find('.owl-stage-outer').children().unwrap();
+        }else if(($(window).width() < 767) && (!program.hasClass('owl-carousel'))){
+             program.owlCarousel({
+                navigation : true,
+                items:1
+            });
+        }
+    });
+    
+    
+    /**
+    * Program courses carousel enabled only on mobile and thablet
+    * @use owl carousel plugin : site/js/owl.carousel.min.js
+    */
+    
+    var tab1 = $('#year2016');    
+    var tab2 = $('#month01');    
+    var active = 0;
+    
+    initOwl(tab1);
+    
+    
+    $(window).resize(function(){
+        if (($(window).width() <= 767) && active == 0) {
+            $('#year2016').tab('show');
+            initOwl(tab1);
+            active = 1;
+        }else if(($(window).width() >= 767) && active == 1){
+            $('#year2016').tab('show');
+            destroyOwl(tab1);
+            destroyOwl(tab2);
+            active = 0;
+        }
+        
+    });
+    
+
+    $('#newsTab a[href="#year2016"]').on('shown.bs.tab', function () {
+        initOwl(tab1);
+    }).on('hide.bs.tab', function () {
+        destroyOwl(tab1);
+    });
+
+    $('#newsTab a[href="#month01"]').on('shown.bs.tab', function () {
+        initOwl(tab2);
+    }).on('hide.bs.tab', function () {
+        destroyOwl(tab2);
+    });
+        
+    
+    
+    function initOwl(el){
+        if ($(window).width() <= 767) {
+             el.owlCarousel({
+                navigation : true,
+                autoHeight : true,
+                items:1
+            });
+        }
+    }
+    
+    function destroyOwl(el){
+        el.trigger('destroy.owl.carousel').removeClass('owl-carousel owl-loaded');
+        el.find('.owl-stage-outer').children().unwrap();
+    }
+    
+    
+    
+  
+    
+    
+    
     
     /**
     * Apply hover class for News Feed items
@@ -52,8 +158,20 @@ $(document).ready(function(){
     */
     
     $('.carousel').carousel({
-      interval: 6000
+      interval: 6000  // slides interval in milliseconds 6000 = 6s
     });
+    
+    
+    /**
+    * Red form when focus label like placeholder
+    */
+     $('.red-form__input')
+        .focus(function(){
+                $(this).parent().addClass('focus');
+        }).blur(function(){
+               $(this).parent().removeClass('focus');
+        });
+    
     
     
 
